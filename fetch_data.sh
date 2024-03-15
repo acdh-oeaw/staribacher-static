f2b4f8e9f406167d3996e3d2926a597479d341f8
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-03-15 15:52:21 +0100

    in case data exists

--- fetch_data.sh
+++ fetch_data.sh
@@ -2,6 +2,6 @@
 echo "fetching transkriptions from data_repo"
 curl -LO https://github.com/fun-with-editions/staribacher-data/archive/refs/heads/main.zip
 unzip ./main
+rm -rf data
 mv ./staribacher-data-main/data .
-
 rm -rf ./main.zip ./staribacher-data-main
