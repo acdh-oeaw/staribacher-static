07c97ff3045499ac04eca631280334a005f25cab
cfhaak <c.f.haak@posteo.de>
2024-03-15 17:08:42 +0100

    simplified this, see #7

--- shellscripts/fetch_editions.sh
+++ shellscripts/fetch_editions.sh
@@ -2,16 +2,13 @@
 # # def vars
 temp_path="./tmp_tmp/"
 zip_file="${temp_path}main.zip"
-fetched_editions_path="${temp_path}staribacher-data-main/data/editions/"
-target_editions_path="./data/editions/"
+fetched_data_path="${temp_path}staribacher-data-main/data"
 # # prepare folder structure
 rm -rf $temp_path
 mkdir $temp_path
 # # fetch data from repo, unzip, and move to destination
 wget https://github.com/fun-with-editions/staribacher-data/archive/refs/heads/main.zip -O $zip_file
 unzip $zip_file -d $temp_path
-# # check for target dir
-if [ ! -d "$target_editions_path" ]; then mkdir "$target_editions_path"; fi
-find $fetched_editions_path -name "*.xml" -exec cp "{}" "$target_editions_path" \;
+cp -r $fetched_data_path ./
 # # remove temp folder
 rm -rf $temp_path
