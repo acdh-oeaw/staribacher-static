42ee267fd23c4a2b021da75011ddcbe82467117e
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-03-25 09:47:37 +0100

    documentation

--- make_ts_index.py
+++ make_ts_index.py
@@ -8,7 +8,10 @@ from acdh_tei_pyutils.utils import extract_fulltext
 from tqdm import tqdm
 from typesense.api_call import ObjectNotFound
 # It needs the OS variable TYPESENSE_API_KEY to be set
-# Additional vars: TYPESENSE_HOST, TYPESENSE_PORT, TYPESENSE_PROTOCOL
+# Additional vars: TYPESENSE_HOST, TYPESENSE_PORT, TYPESENSE_PROTOCOL.
+# Default: http://typesense.acdh-dev.oeaw.ac.at/, "https", "443"
+# TYPESENSE_READ_KEY VSMye2GdRZvBRkpYnjXMUfZpOCiSOFLO
+
 from acdh_cfts_pyutils import TYPESENSE_CLIENT as client
 # from acdh_cfts_pyutils import CFTS_COLLECTION
 
