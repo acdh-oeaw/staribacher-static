ebe8bb45b7431864d9dbab725860e3c34b9b3710
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-05-28 19:04:27 +0200

    adding mentions to index

---
+++ add_mentions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import glob
+from acdh_tei_pyutils.tei import TeiReader
+import lxml.etree as ET
+from tqdm import tqdm
+
+files = sorted(glob.glob("./data/editions/*.xml"))
+
+
+mentions = {}
+
+sender = set()
+receiver = set()
+send_place = set()
+for edition in tqdm(files, total=len(files)):
+    doc = TeiReader(x)
+    persons = doc.any_xpath('.//tei:rs[type=""person"]')
+    persons_id = [person.attrib["ref"] for person in persons if person.attrib["ref"] not in persons]
+    for person in persons_id:
+        if person in mentions:
+            mentions[person].append(edition)
+        else:
+             mentions[person] = [edition]
+
+print("writing sender, receiver, send place infos as tei:state into index files")
+doc = TeiReader("./data/indices/listperson.xml")
+print(mentions)
+#doc.tree_to_file("./data/indices/listplace.xml")
