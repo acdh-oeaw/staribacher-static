e8eda349cc64af16b3368b5236a355681184f1fb
csae8092 <peter.andorfer@oeaw.ac.at>
2024-06-08 08:20:00 +0200

    checked in favicons, rm favicon build script; rm notgroups from edition files, added process_data.sh

---
+++ pyscripts/rm_notegrps.py
@@ -0,0 +1,15 @@
+import glob
+from tqdm import tqdm
+from acdh_tei_pyutils.tei import TeiReader
+
+files = sorted(glob.glob("./data/editions/*.xml"))
+print(f"removing noteGrp from {len(files)} edition files")
+
+for x in tqdm(files, total=len(files)):
+    try:
+        doc = TeiReader(x)
+    except:  # noqa
+        continue
+    for bad in doc.any_xpath(".//tei:back//tei:noteGrp"):
+        bad.getparent().remove(bad)
+    doc.tree_to_file(x)
---
+++ shellscripts/process_data.sh
@@ -0,0 +1,16 @@
+echo "add xml:ids"
+add-attributes -g "./data/editions/*.xml" -b "https://staribacher.acdh.oeaw.ac.at"
+add-attributes -g "./data/meta/*.xml" -b "https://staribacher.acdh.oeaw.ac.at"
+add-attributes -g "./data/indices/*.xml" -b "https://staribacher.acdh.oeaw.ac.at"
+
+echo "denormalize indices"
+denormalize-indices -f "./data/editions/*.xml" -i "./data/indices/*.xml" -m ".//*[@ref]/@ref | .//*/@source" -x ".//tei:titleStmt/tei:title[@type='main']/text()"
+
+echo "remove notegrps from index entries in editions"
+python pyscripts/rm_notegrps.py
+
+echo "populate html/js-data/calendarData.js"
+python python pyscripts/make_calendar_data.py
+
+echo "populate fulltext-search index"
+echo "call the correct srcipt" 
