d16828bcf97f7260d4036725e6c026da608dd46c
rausch-supola <michaela.rauschsupola@gmail.com>
2024-08-28 13:17:25 +0200

    show loading image during PDF download

--- html/js/osd_scroll.js
+++ html/js/osd_scroll.js
@@ -267,11 +267,11 @@ function isInViewportAll(element) {
 
 /* 
 ##################################################################
-function for generating pdf with all scans of current day
+functions for generating pdf with all scans of current day
 ##################################################################
 */
 
-async function getdu(imgUrl) {
+async function getDU(imgUrl) {
     let blob = await fetch(imgUrl).then(r => r.blob());
     let dataUrl = await new Promise(resolve => {
       let reader = new FileReader();
@@ -298,7 +298,7 @@ async function generatePDF() {
     // Create an array of promises to fetch all images
     const promises = Array.from(element_a).map(async (element) => {
         let imgUrl = element.getAttribute("source");
-        return getdu(imgUrl); // Get the dataUrl for each image
+        return getDU(imgUrl); // Get the dataUrl for each image
     });
 
     try {
@@ -314,7 +314,49 @@ async function generatePDF() {
         });
 
         doc.save(createFilename() + ".pdf");
+        // Set cookie so that loading image will vanish after successful download
+        setCookie("downloadStarted", 1, Math.floor(Date.now() / 1000) + 20, '/', "", false, false);
     } catch (error) {
         console.error('Error generating PDF:', error);
     }
 }
+
+/* 
+##################################################################
+Add loading animation during PDF download
+##################################################################
+*/
+  var setCookie = function(name, value, expiracy) {
+    var exdate = new Date();
+    exdate.setTime(exdate.getTime() + expiracy * 1000);
+    var c_value = encodeURIComponent(value) + ((expiracy == null) ? "" : "; expires=" + exdate.toUTCString());
+    document.cookie = name + "=" + c_value + '; path=/';
+  };
+  
+  var getCookie = function(name) {
+    let ARRcookies = document.cookie.split(";");
+    for (let i = 0; i < ARRcookies.length; i++) {
+      let x = ARRcookies[i].substring(0, ARRcookies[i].indexOf("="));
+      let y = ARRcookies[i].substring(ARRcookies[i].indexOf("=") + 1);
+      x = x.replace(/^\s+|\s+$/g, "");
+      if (x == name) {
+        console.log(x, ' ', y);
+        return y ? decodeURI(decodeURIComponent(y.replace(/\+/g, ' '))) : y;
+      }
+    }
+  };
+  
+  $('#downloadLink').click(function() {
+    $('#fader').css('display', 'block');
+    setCookie('downloadStarted', 0, 100); // Expiration could be anything... As long as we reset the value
+    setTimeout(checkDownloadCookie, 1000); // Initiate the loop to check the cookie.
+  });
+  var downloadTimeout;
+  var checkDownloadCookie = function() {
+    if (getCookie("downloadStarted") == 1) {
+      setCookie("downloadStarted", "false", 100); //Expiration could be anything... As long as we reset the value
+      $('#fader').css('display', 'none');
+    } else {
+      downloadTimeout = setTimeout(checkDownloadCookie, 1000); //Re-run this function in 1 second.
+    }
+  };
